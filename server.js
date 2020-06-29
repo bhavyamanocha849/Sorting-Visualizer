865c7d0bbb4e39edb283f6d743046a03684ee783
Kunal202 <kunalchandel212@gmail.com>
2020-06-29 16:44:01 +0530

    Ready for deployment

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+var compresssion = require('compression')
 
 app.use(express.static(__dirname + "/views/"))
+app.use(compresssion())
 router.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
 app.use('/', router)
-app.listen(3000, () => {
+app.listen(process.env.PORT||3000, () => {
     console.log('You are connected on http://localhost:3000/')
 })
